8b2d19c1bd72188d238caea79aeb934995835b90
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-11-03 23:22:37 +0200

    add lesson 33

--- project_mDB/project/js/script.js
+++ project_mDB/project/js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -26,55 +12,74 @@ const movieDB = {
 
 const adv = document.querySelectorAll('.promo__adv img'),
     genre = document.querySelector('.promo__genre'),
-    bg = document.querySelector('.promo__bg'),
-    movieList = document.querySelector('.promo__interactive-list')
+    poster = document.querySelector('.promo__bg'),
+    movieList = document.querySelector('.promo__interactive-list'),
+    addForm = document.querySelector('form.add'),
+    addInput = addForm.querySelector('.adding__input'),
+    checkbox = addForm.querySelector('[type="checkbox"]');
 
-adv.forEach(item => {
-    item.remove();
-} );
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-genre.textContent = 'ДРАМА';
+    let newFilm = addInput.value;
+    const favorite = checkbox.checked;
 
-bg.style.backgroundImage = "url('img/bg.jpg')";
+    if(newFilm) {
 
-movieList.innerHTML = "";
+        if(newFilm.length > 21) {
+            newFilm = `${newFilm.substring(0, 22)}...`
+        }
 
-movieDB.movies.sort();
+        if(favorite) {
+            console.log('Добавляем любимый фильм');
+        }
+        movieDB.movies.push(newFilm);
+        sortArr(movieDB.movies);
+        createMovieList(movieDB.movies, movieList)
 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
+    }
+
+    event.target.reset();
+});
+
+const deleteAdv = (arr) => {
+    arr.forEach(item => {
+        item.remove();
+    });
+};
+
+const makeChanges = () => {
+    genre.textContent = 'ДРАМА';
+    poster.style.backgroundImage = "url('img/bg.jpg')";
+};
+
+const sortArr = (arr) => {
+    arr.sort();
+};
+
+
+function  createMovieList(films, parent) {
+    parent.innerHTML = "";
+    sortArr(films);
+
+    films.forEach((film, i) => {
+        parent.innerHTML += `
         <li class="promo__interactive-item">${i + 1}. ${film}
             <div class="delete"></div>
         </li>
     `;
-});
+    });
 
-/* Задания на урок:
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" -
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение:
-"Добавляем любимый фильм"
-5) Фильмы должны быть отсортированы по алфавиту */
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
 
-const btn = document.querySelector('button'),
-    input = document.querySelector('.adding__input'),
-    deleted = document.querySelectorAll('.delete');
+            createMovieList(films, parent);
+        })
+    })
+}
 
-btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    movieDB.movies.push(input.value);
-    console.log(movieDB.movies);
-
-})
-
-
-// deleted.forEach((item, i) => {
-//     item.addEventListener(click, () => {
-//         item.remove()
-//     })
-// })
+deleteAdv(adv);
+makeChanges();
+createMovieList(movieDB.movies, movieList);
