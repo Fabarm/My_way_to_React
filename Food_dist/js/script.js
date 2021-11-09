56a7a93eeb911c8f6cd678228979f4e6fbf0066c
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-11-09 11:56:14 +0200

    add lesson 49

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -144,12 +144,13 @@ window.addEventListener('DOMContentLoaded', function () {
     // Используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, discr, price, parentSelector) {
+        constructor(src, alt, title, discr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.discr = discr;
             this.price = price;
+            this.classes = classes
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUA();
@@ -161,8 +162,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
         render() {
             const element = document.createElement('div');
-            element.innerHTML = `
-                <div class="menu__item">
+            if(this.classes.length === 0){
+                this.element = 'menu__item'
+                element.classList.add(this.element)
+            }else{
+                this.classes.forEach(className => element.classList.add(className))
+            }
+
+            element.innerHTML = `                
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.discr}</div>
@@ -170,8 +177,7 @@ window.addEventListener('DOMContentLoaded', function () {
                     <div class="menu__item-price">
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
-                </div>
+                    </div>                
             `;
             this.parent.append(element);
         }
